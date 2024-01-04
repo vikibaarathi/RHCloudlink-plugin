110eed7fb05f29f0e2f295a1a30613309a112e84
Viki Baarathi <viki.baarathi@gmail.com>
2024-01-04 08:00:53 +0800

    Removed alchemy class

--- cloudlink.py
+++ cloudlink.py
@@ -5,23 +5,6 @@ from sqlalchemy.ext.declarative import DeclarativeMeta
 from RHUI import UIField, UIFieldType
 from .datamanager import ClDataManager
 
-class AlchemyEncoder(json.JSONEncoder):
-
-    def default(self, obj):
-        if isinstance(obj.__class__, DeclarativeMeta):
-            # an SQLAlchemy class
-            fields = {}
-            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-                data = obj.__getattribute__(field)
-                try:
-                    json.dumps(data) # this will fail on non-encodable values, like other classes
-                    fields[field] = data
-                except TypeError:
-                    fields[field] = None
-            # a json-encodable dict
-            return fields
-
-        return json.JSONEncoder.default(self, obj)
 class CloudLink():
     CL_VERSION = "1.0.0"
     CL_API_ENDPOINT = "https://api.rhcloudlink.com"
