ec6dde7df5105822d9517d78cdd42fc7637772fe
Viki Baarathi <viki.baarathi@gmail.com>
2026-03-11 22:44:56 +0800

    Prepared a config file for staging configuration. not really important

--- custom_plugins/cloudlink/config.example.py
+++ custom_plugins/cloudlink/config.example.py
@@ -6,12 +6,6 @@ config.py is gitignored and will never be committed.
 """
 import os
 
-# Production:
-# CL_API_ENDPOINT = "https://api.rhcloudlink.com"
-
-# Staging:
-# CL_API_ENDPOINT = "https://u9d3pvkmii.ap-southeast-1.awsapprunner.com"
-
 CL_API_ENDPOINT = os.environ.get(
     "CL_API_ENDPOINT",
     "https://api.rhcloudlink.com"
